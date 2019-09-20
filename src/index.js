4ac12661ee86840fb0ab70c781bea5c16cf70643
Merdan Durdiyev <merdan.durdiyev@gmail.com>
2019-09-20 17:35:01 +0500

    Ready to play, allows to move back in time, to previous moves

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber+1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
 
@@ -80,6 +80,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         })
     }    
@@ -87,7 +88,7 @@ class Game extends React.Component {
     render() {
 
         const history = this.state.history;
-        const current = history[history.length-1];
+        const current = history[this.state.stepNumber];
         const winner = CalculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
